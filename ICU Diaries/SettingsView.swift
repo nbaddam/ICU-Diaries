e495c03495361ddab08af3ad53e944549acd4b7f
Zaher Hage <zhhage@umich.edu>
2021-11-10 13:58:11 -0500

    connect family member to patient

--- ICU Diaries/SettingsView.swift
+++ ICU Diaries/SettingsView.swift
@@ -11,7 +11,8 @@ import Firebase
 
 struct SettingsView: View {
     @State private var SignOutSuccess: Bool? = false
-    
+    @State var code = ""
+    @State var codes: [String] = []
     var body: some View {
         VStack {
             Text("Settings")
@@ -23,6 +24,43 @@ struct SettingsView: View {
             NavigationLink(destination: ContentView().navigationBarBackButtonHidden(true), tag: true, selection: $SignOutSuccess) {
                 EmptyView()
             }
+//Add a code assignemnt button
+            Text("Patient Code:")
+                TextField(
+                    "Type Here",
+                    text: $code)
+                    .disableAutocorrection(true)
+                    .cornerRadius(5)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.bottom, 10)
+            Text("Assign")
+                .padding(10)
+                .padding(.leading, 15)
+                .padding(.trailing, 15)
+                .background(Color.blue)
+                .cornerRadius(2)
+                .foregroundColor(.white)
+                .onTapGesture {
+                    let db = Firestore.firestore()
+                    let user_code = db.collection("users").document(Auth.auth().currentUser!.uid)
+                    db.collection("codes").getDocuments() { (querySnapshot, error) in
+                        if let error = error {
+                            print("Error getting documents: \(error)")
+                        }
+                        else {
+                            for document in querySnapshot!.documents {
+                                if(document.documentID == code){
+                                    user_code.updateData(["code" : code])
+                                    self.codes += [code]
+                                    break
+                                }
+                            }//prints all documents in the firebase
+                            if !codes.contains(code){
+                                print("code not found")
+                            }
+                        }//else
+                    }//getDocuments
+                }//onTap
             Text("Sign Out")
                 .navigationBarTitle(Text(""), displayMode: .inline)
                 .navigationBarHidden(true)
@@ -40,7 +78,7 @@ struct SettingsView: View {
                     catch {
                         print("didnt work try again")
                     }
-                }
+                }//onTap
             
             Spacer()
             
