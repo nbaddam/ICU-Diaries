f3ad23535e7067769931ab152fabd50b7e742b7b
Nitya Reddy Baddam <nbaddam@umich.edu>
2021-11-15 11:25:55 -0500

    Fixing timeline spacing

--- ICU Diaries/Timeline.swift
+++ ICU Diaries/Timeline.swift
@@ -14,33 +14,31 @@ struct Timeline: View {
    @ObservedObject private var viewModel = PostViewModel()
 
     var body: some View {
-        NavigationView {
-            VStack {
-                Text("Timeline")
-                    .font(.largeTitle)
-                    .fontWeight(.semibold)
-                Spacer()
-                GeometryReader { geometry in
-                    List {
-                        ForEach(viewModel.posts, id: \.id) {(post) in
-                            if !post.imageName.isEmpty {
-                                PostView(hasImage: true, hasVideo: false, hasAudio: false, post: post, screenWidth: geometry.size.width)
-                            }
-                            else if !post.videoName.isEmpty {
-                                PostView(hasImage: false, hasVideo: true, hasAudio: false, post: post, screenWidth: geometry.size.width)
-                            }
-                            else {
-                                PostView(hasImage: false, hasVideo: false, hasAudio: false, post: post, screenWidth: geometry.size.width)
-                            }
+        VStack {
+            Text("Timeline")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            Spacer()
+            GeometryReader { geometry in
+                List {
+                    ForEach(viewModel.posts, id: \.id) {(post) in
+                        if !post.imageName.isEmpty {
+                            PostView(hasImage: true, hasVideo: false, hasAudio: false, post: post, screenWidth: geometry.size.width)
                         }
-                    }.onAppear() {
-                        self.viewModel.posts.removeAll()
-                        self.viewModel.getData()
+                        else if !post.videoName.isEmpty {
+                            PostView(hasImage: false, hasVideo: true, hasAudio: false, post: post, screenWidth: geometry.size.width)
+                        }
+                        else {
+                            PostView(hasImage: false, hasVideo: false, hasAudio: false, post: post, screenWidth: geometry.size.width)
+                        }
                     }
+                }.onAppear() {
+                    self.viewModel.posts.removeAll()
+                    self.viewModel.getData()
                 }
-                NavigationLink(destination: FilterView().navigationBarBackButtonHidden(false)) {
-                    Text("Filter")
-                }
+            }
+            NavigationLink(destination: FilterView().navigationBarBackButtonHidden(false)) {
+                Text("Filter")
             }
         }
     }
